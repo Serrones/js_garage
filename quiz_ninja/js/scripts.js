f00d03a7cca0607351edcc684e6ea7f8168abf7c
Fábio Serrão <fabio.serrones@seno-ti.com.br>
2018-10-10 17:31:00 -0300

    Exercises from JavaScript: Novice to Ninja - Chapter 4

--- quiz_ninja/js/scripts.js
+++ quiz_ninja/js/scripts.js
@@ -18,17 +18,35 @@ var quiz = [
 
 var score = 0 // initialize score
 
-for(var i=0, max=quiz.length; i<max; i++){
-    // get answer from user
-    var answer = prompt(quiz[i][0]); // quiz[i][0] is the ith question
-    // check if answer is correct
-    if(answer === quiz[i][1]){ // quiz[i][1] is the ith answer
-        alert("Correct!");
-        // increase score by 1
-        score++;
-    } else {
-        alert("Wrong!");
+play(quiz);
+
+function play(quiz){
+    // main game loop
+    for(var i=0, question, answer, max=quiz.length; i<max; i++){
+        question = quiz[i][0];
+        answer = ask(question);
+        check(answer);
+    }
+    // end of main game loop
+    gameOver();
+
+    function ask(question) {
+        return prompt(question); // quiz[i][0] is the ith questions
+    }
+
+    function check(answer) {
+        if(answer === quiz[i][1]){ // quiz[i][1] is the ith answer
+            alert("Correct!");
+            // increase score by 1
+            score++;
+        } else {
+            alert("Wrong!");
+        }
+    }
+
+    function gameOver(){
+        // inform the player that the game has finished and tell
+        // them how many points they have scored
+        alert("Game Over, you scored " + score + " points");
     }
 }
-
-alert("Game Over, you scored " + score + " points");
