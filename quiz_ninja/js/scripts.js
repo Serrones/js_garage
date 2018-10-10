fcb94d5d10635239d156011c2c02bd91362884d1
Fábio Serrão <fabio.serrones@seno-ti.com.br>
2018-10-10 12:03:15 -0300

    Exercises from JavaScript: Novice to Ninja - Chapters 1 and 2

---
+++ quiz_ninja/js/scripts.js
@@ -0,0 +1,10 @@
+
+// Chapter 1
+// welcome the user
+alert("Welcome to Quiz Ninja!");
+
+// Chapter 2
+var question = "What is Superman's real name?";
+var answer = prompt(question);
+alert("You answered " + answer);
+
